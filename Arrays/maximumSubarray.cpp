e76867a017eabcc5f9cbb8ef28c9aae7da29b64f
nagatejamanikanta <nagatejamanikanta@gmail.com>
2025-10-27 20:08:09 +0530

    solved maximum subarray problem

--- Arrays/maximumSubarray.cpp
+++ Arrays/maximumSubarray.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        //manikanta
         int sum = 0 ;
         int maxsum = INT_MIN ;
         for(int i = 0 ; i < nums.size() ; i++){
